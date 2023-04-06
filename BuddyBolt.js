78e0626f346a571517f8a8c209a852fe90380d89
Greg Funk <gregory.c.funk@gmail.com>
2023-04-05 21:06:13 -0400

    puts user text on the screen

--- BuddyBolt.js
+++ BuddyBolt.js
@@ -3,7 +3,29 @@ const recognition = new webkitSpeechRecognition();
 recognition.onresult = function(event) {
   const transcript = event.results[event.results.length - 1][0].transcript;
   console.log(transcript);
-  document.getElementById('output').innerHTML = transcript;
+    // document.getElementById('output').innerHTML = transcript;
+
+    // Get the chat box element
+    const chatBox = document.querySelector('.chat');
+
+    // Create the user message element
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.classList.add('user-message');
+
+    const messageTextElement = document.createElement('div');
+    messageTextElement.classList.add('message-text');
+    messageTextElement.innerText = transcript;
+    messageElement.appendChild(messageTextElement);
+
+    const avatarElement = document.createElement('img');
+    avatarElement.src = "user-avatar.png";
+    avatarElement.alt = "User Avatar";
+    avatarElement.classList.add('avatar');
+    messageElement.appendChild(avatarElement);
+
+    // Add the message element to the chat box
+    chatBox.appendChild(messageElement);
 }
 
 recognition.onerror = function(event) {
